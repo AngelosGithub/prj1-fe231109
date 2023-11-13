db4583f0ded9f5ece29e70dad4909b75b9863565
Angelo <ljy6037@gmail.com>
2023-11-13 10:35:56 +0900

    231113 회원 탈퇴 모달 제작

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,12 +1,21 @@
-import { useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   Button,
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -14,6 +23,11 @@ export function MemberView() {
   const [member, setMember] = useState(null);
   // /member?id=userid
   const [params] = useSearchParams();
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  const navigate = useNavigate();
+  const toast = useToast();
 
   useEffect(() => {
     axios
@@ -23,6 +37,39 @@ export function MemberView() {
 
   if (member === null) {
     return <Spinner />;
+  }
+
+  function handleDelete() {
+    // axios
+    // delete /api/member?id=userid
+    // ok -> home으로, toast 띄우기
+    // error -> toast 띄우기
+    // finally -> modal 닫기
+    axios
+      .delete("/api/member?" + params.toString())
+      .then(() => {
+        toast({
+          description: "탈퇴 되었습니다",
+          status: "success",
+        });
+        navigate("/");
+
+        // todo : 로그아웃 기능 추가 하기
+      })
+      .catch((error) => {
+        if (error.response.status === 401 || error.response.status === 403) {
+          toast({
+            description: "권한이 없습니다",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "문제가 발생했습니다",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => onClose());
   }
 
   return (
@@ -38,7 +85,25 @@ export function MemberView() {
         <Input type="text" value={member.email} readOnly />
       </FormControl>
       <Button colorScheme="green">수정</Button>
-      <Button colorScheme="red">삭제</Button>
+      <Button colorScheme="red" onClick={onOpen}>
+        탈퇴
+      </Button>
+
+      {/*탈퇴 모달*/}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>탈퇴 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>탈퇴 하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleDelete} colorScheme="red">
+              탈퇴
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }

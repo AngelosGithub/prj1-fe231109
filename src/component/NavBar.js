87c0ef8f8388c69e711a71587899333f0d531621
Angelo <ljy6037@gmail.com>
2023-11-14 09:54:17 +0900

    231114 로그아웃 버튼 제작

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,8 +1,14 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, theme } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export function NavBar() {
   const navigate = useNavigate();
+
+  function handleLogout() {
+    axios.post("/api/member/logout").then(() => console.log("logout"));
+  }
+
   return (
     <Flex>
       <Button onClick={() => navigate("/")}>home</Button>
@@ -10,6 +16,7 @@ export function NavBar() {
       <Button onClick={() => navigate("/signup")}>signup</Button>
       <Button onClick={() => navigate("/member/list")}>회원목록</Button>
       <Button onClick={() => navigate("/login")}>Login</Button>
+      <Button onClick={handleLogout}>Logout</Button>
     </Flex>
   );
 }

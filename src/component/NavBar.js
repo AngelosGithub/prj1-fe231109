a235185c48d62af9512e29fc9c22a0cc439d19b2
Angelo <ljy6037@gmail.com>
2023-11-14 15:16:51 +0900

    231114 로그인, 로그아웃 기능 추가 완료

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,12 +1,26 @@
-import { Button, Flex, theme } from "@chakra-ui/react";
+import { Button, Flex, theme, useConst, useToast } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useContext } from "react";
+import { LoginContext } from "../App";
 
 export function NavBar() {
+  // 세션이 서버에 있기때문에 클라이언트에서 사용하기 위해 쓰는 코드
+  const { fetchLogin, login, isAuthenticated } = useContext(LoginContext);
+  const toast = useToast();
   const navigate = useNavigate();
 
   function handleLogout() {
-    axios.post("/api/member/logout").then(() => console.log("logout"));
+    axios
+      .post("/api/member/logout")
+      .then(() => {
+        toast({
+          description: "로그아웃 되었습니다",
+          status: "info",
+        });
+        navigate("/");
+      })
+      .finally(() => fetchLogin());
   }
 
   return (
